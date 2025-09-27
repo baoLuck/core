52f819d5e89b390c875fcb0768d214b7bae427ff
baoLuck <vitalymayer1706@gmail.com>
2025-09-27 13:47:35 +0300

    fix

---
+++ test/contract_qbond.cpp
@@ -0,0 +1,87 @@
+#define NO_UEFI
+
+#include "contract_testing.h"
+
+std::string assetNameFromInt64(uint64 assetName);
+const id devAddress = ID(_B, _O, _N, _D, _D, _J, _N, _U, _H, _O, _G, _Y, _L, _A, _A, _A, _C, _V, _X, _C, _X, _F, _G, _F, _R, _C, _S, _D, _C, _U, _W, _C, _Y, _U, _N, _K, _M, _P, _G, _O, _I, _F, _E, _P, _O, _E, _M, _Y, _T, _L, _Q, _L, _F, _C, _S, _B);
+const id adminAddress = ID(_B, _O, _N, _D, _A, _A, _F, _B, _U, _G, _H, _E, _L, _A, _N, _X, _G, _H, _N, _L, _M, _S, _U, _I, _V, _B, _K, _B, _H, _A, _Y, _E, _Q, _S, _Q, _B, _V, _P, _V, _N, _B, _H, _L, _F, _J, _I, _A, _Z, _F, _Q, _C, _W, _W, _B, _V, _E);
+const id testAddress = ID(_H, _O, _G, _T, _K, _D, _N, _D, _V, _U, _U, _Z, _U, _F, _L, _A, _M, _L, _V, _B, _L, _Z, _D, _S, _G, _D, _D, _A, _E, _B, _E, _K, _K, _L, _N, _Z, _J, _B, _W, _S, _C, _A, _M, _D, _S, _X, _T, _C, _X, _A, _M, _A, _X, _U, _D, _F);
+
+class QBondChecker : public MSVAULT
+{
+public:
+    HashMap<uint16, MBondInfo, QBOND_MAX_EPOCH_COUNT> getInfo()
+    {
+        return _epochMbondInfoMap;
+    }
+};
+
+class ContractTestingQBond : protected ContractTesting
+{
+public:
+    ContractTestingQBond()
+    {
+        initEmptySpectrum();
+        initEmptyUniverse();
+        INIT_CONTRACT(MSVAULT);
+        callSystemProcedure(MSVAULT_CONTRACT_INDEX, INITIALIZE);
+    }
+
+    QBondChecker* getState()
+    {
+        return (QBondChecker*)contractStates[MSVAULT_CONTRACT_INDEX];
+    }
+
+    bool loadState(const CHAR16* filename)
+    {
+        return load(filename, sizeof(MSVAULT), contractStates[MSVAULT_CONTRACT_INDEX]) == sizeof(MSVAULT);
+    }
+
+    void stake(const int64_t& quMillions, const id& staker)
+    {
+        MSVAULT::Stake_input input{ quMillions };
+        MSVAULT::Stake_output output;
+        invokeUserProcedure(MSVAULT_CONTRACT_INDEX, 1, input, output, staker, quMillions * 2000000);
+    }
+
+    QSWAP::TeamInfo_output teamInfo()
+    {
+        return {};
+    }
+
+    void bedinEpoch(bool expectSuccess = true)
+    {
+        callSystemProcedure(MSVAULT_CONTRACT_INDEX, BEGIN_EPOCH, expectSuccess);
+    }
+
+    void stakers()
+    {
+        printf("population %lld\n", getState()->getInfo().population());
+        printf("stakers %lld\n", getState()->getInfo().value(0).stakersAmount);
+        for (int i = 0; i < 1024; i++)
+        {
+            if (!getState()->getInfo().isEmptySlot(i))
+            {
+                printf("%s\n", assetNameFromInt64(getState()->getInfo().value(i).name).c_str());
+                printf("stakers %lld\n", getState()->getInfo().value(i).stakersAmount);
+            }
+        }
+    }
+};
+
+
+TEST(ContractQBond, IssueAsset)
+{
+    ContractTestingQBond qbond;
+    qbond.stakers();
+    qbond.bedinEpoch();
+    //increaseEnergy(testAddress, 50 * 2000000);
+    qbond.stake(50, testAddress);
+    qbond.stakers();
+   /* EXPECT_EQ(_commissionFreeAddresses.population(), 1);*/
+}
+
+TEST(ContractQBond, CleanupCollections)
+{
+    EXPECT_EQ(1, 1);
+}
