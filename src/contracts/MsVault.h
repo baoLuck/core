2a9ee2d910c2637a091188655c068f2fcdeb1d9c
baoLuck <vitalymayer1706@gmail.com>
2025-09-30 00:04:53 +0300

    fix

--- src/contracts/MsVault.h
+++ src/contracts/MsVault.h
@@ -5,7 +5,7 @@ constexpr uint64 QBOND_MBOND_PRICE = 1000000ULL;
 constexpr uint64 QBOND_MAX_QUEUE_SIZE = 10ULL;
 constexpr uint64 QBOND_MIN_MBONDS_TO_STAKE = 10ULL;
 constexpr sint64 QBOND_MBONDS_EMISSION = 1000000000LL;
-constexpr uint16 QBOND_START_EPOCH = 182;
+constexpr uint16 QBOND_START_EPOCH = 170;
 
 constexpr uint64 QBOND_STAKE_FEE_PERCENT = 50; // 0.5%
 constexpr uint64 QBOND_TRADE_FEE_PERCENT = 3; // 0.03%
