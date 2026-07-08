0300cb285260e71a31c9a4d22be47528562f5f9a
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2026-07-08 15:06:53 +0200

    Revert "add comment for GGWP epoch guard"
    This reverts commit e70230100e0858ce1aa400bab70f6af157da392c.

--- src/contracts/GGWP.h
+++ src/contracts/GGWP.h
@@ -962,9 +962,6 @@ struct WOLFPACK : public ContractBase
     {
         if (qpi.epoch() < 221)
         {
-            // Before epoch 221, this procedure should be unknown. processTickTransactionContractProcedure will call POST_INCOMING_TRANSFER
-            // if a tx with amount > 0 is invoking an unknown procedure. So before epoch 221, POST_INCOMING_TRANSFER should be invoked here as well,
-            // however, this can be skipped because the contract does not define POST_INCOMING_TRANSFER.
             return;
         }
 
